1a3d85ae0b82fc56e08136ebc6e8aaca3e654824
WDOVELU <lujiajie010@126.com>
2020-03-03 17:19:34 +0100

    Site updated: 2020-03-03 17:19:31

--- CGhtml/A01/draw.js
+++ CGhtml/A01/draw.js
@@ -1,17 +1,34 @@
 function draw() {
-	// line(x1,y1, x2,y2)
-	// draws a line from a point at Normalized screen coordinates x1,y1 to Normalized screen coordinates x2,y2
-
-	// Here there are only three lines command, but to obtain the car you will need 8 of them
-	line(0.4, 0.4,-0.4,-0.4);
-	line(0.4,-0.4,-0.4, 0.4);
-	line(0.4,-0.3,-0.4,-0.3);
+	line(-0.5, -0.3,0.5, -0.3);
+	line(0.5, -0.3,0.5,  0.0);
+	line(0.5,  0.0,0.3,  0.1);
+	line(0.3,  0.1,0.3,  0.3);
+	line(0.3,  0.3, -0.2,  0.3);
+	line(-0.2,  0.3,-0.2,  0.1);
+	line(-0.2,  0.1,-0.5,  0.0);
+	line(-0.5,  0.0,-0.5, -0.3);
 }
-
-function draw2() {
-	// this function is to draw a circle
-	for(i = 0; i < 128; i++) {
-		y = (i - 64)  / 127.0 * 1.6;
-		line(0.4, y,-0.4, y);
+function draw2(x0,y0,radius,prop,rotation) {
+	var n = 128;
+	var xx0 = radius * Math.cos(0*2*Math.PI/n+rotation)+x0;
+	var yy0 = radius * Math.sin(0*2*Math.PI/n+rotation)+y0;
+	for(i = 1; i <= (prop * n); i++) {
+		x = radius * Math.cos(i*2*Math.PI/n+rotation)+x0;
+		y = radius * Math.sin(i*2*Math.PI/n+rotation)+y0;
+		line(xx0,yy0,x,y);// vU = vU.concat([xx0,yy0,x,y])
+		xx0=x; yy0=y;
 	}
 }
+function drawyinyang() {
+	draw2(0,0,0.8,1,0);
+	draw2(0,0.4,0.1,1,0);
+	draw2(0,-0.4,0.1,1,0);
+	var rotation = 0.5*Math.PI;
+	draw2(0,0.4,0.4,0.5,rotation);
+	var rotation = 1.5*Math.PI;
+	draw2(0,-0.4,0.4,0.5,rotation);
+}
+
+
+
+
